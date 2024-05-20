024c6516282c3099f3901e193c80d9f897fadf94
Priyamur <144650236+Priyamur@users.noreply.github.com>
2024-05-20 16:15:37 +0530

    Update EnrollmentRepository.cs
    get enrollment repository

--- LXP.Data/Repository/EnrollmentRepository.cs
+++ LXP.Data/Repository/EnrollmentRepository.cs
@@ -31,6 +31,40 @@ namespace LXP.Data.Repository
             {
                 return _lXPDbContext.Enrollments.Any(enrollment => enrollment.LearnerId == LearnerId && enrollment.CourseId == CourseId);
             }
+
+              public object GetCourseandTopicsByLearnerId(Guid learnerId)
+  {
+      var result = from enrollment in _lXPDbContext.Enrollments
+                   where enrollment.LearnerId == learnerId
+                   select new
+                   {
+                       enrolledCourseId = enrollment.CourseId,
+                       enrolledCoursename=enrollment.Course.Title,
+                       enrolledcoursedescription = enrollment.Course.Description,
+                       enrolledcoursecategory = enrollment.Course.Catagory.Category,
+                       enrolledcourselevels = enrollment.Course.Level.Level,
+                       Thumbnailimage = String.Format("{0}://{1}{2}/wwwroot/CourseThumbnailImages/{3}",
+                       _contextAccessor.HttpContext.Request.Scheme,
+                       _contextAccessor.HttpContext.Request.Host,
+                       _contextAccessor.HttpContext.Request.PathBase,
+                       enrollment.Course.Thumbnail),
+
+                       Topics = (from topic in _lXPDbContext.Topics
+                                 where topic.CourseId == enrollment.CourseId && topic.IsActive == true
+                                 select new
+                                 {
+                                     TopicName = topic.Name,
+                                     TopicDescription = topic.Description,
+                                     TopicId = topic.TopicId,
+                                     TopicIsActive = topic.IsActive,
+
+
+                                 }).ToList()
+                   };
+      return result;
+
+
+  }
         }
     }
 
